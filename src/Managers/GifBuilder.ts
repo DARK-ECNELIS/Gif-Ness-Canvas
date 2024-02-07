5d628916671450dc7c122ac6616cd6344d6b2945
DARK-ECNELIS <darkecnelis@gmail.com>
2024-02-06 21:01:06 -0400

    update to ness-canvas 2.4.0

--- src/Managers/GifBuilder.ts
+++ src/Managers/GifBuilder.ts
@@ -1,7 +1,8 @@
 import { writeFileSync } from "fs";
-import { Canvas, CanvasRenderingContext2D, Image } from "canvas";
-// import { CanvasImage, CustomColor, Shape, ImagelocationOption, DrawlocationOption, FrameOption, ExpOption, FrameContent, TextOption, NessBuilder, FrameType } from "ness-canvas";
+import { Canvas, Image } from "canvas";
+import { CanvasImage, CustomColor, Shape, ImagelocationOption, DrawlocationOption, FrameOption, ExpOption, FrameContent, TextOption, NessBuilder, FrameType, IntRange, ShapeLoad, Axis } from "ness-canvas";
 import { gifExtractor, progressBar } from "../function";
+import { ExpColor, LoadingOption } from "ness-canvas/Interfaces";
 
 const GIFEncoder = require('gif-encoder-2')
 
@@ -10,7 +11,7 @@ export default class GifBuilder {
   protected declare canvas: Canvas;
   // protected declare context: CanvasRenderingContext2D;
 
-  // private framePlacement = [];
+  private framePlacement = [];
   
   constructor (width, height) {
     this.canvas = new Canvas(width, height)
@@ -22,7 +23,7 @@ export default class GifBuilder {
    * @param outline Line size (default 3)
    * @param color Line color (default #FFFFFF)
    */
-  public setCornerRadius(radius: number, outline: number = 3, color: CustomColor = "#FFFFFF"): this {
+  public setCornerRadius(radius: number, outline?: number, color?: CustomColor): this {
     this.framePlacement.push({id: 0, radius, outline, color})
     return this;
   };
@@ -31,7 +32,7 @@ export default class GifBuilder {
    * Sets the canvas background.
    * @param imageColor The image to set (no link, use loadImage() from canvas) or a custom color (Valid syntaxes: #hex | rgb | rgba | colorName | CanvasGradient | CanvasPattern)
    */
-  public setBackground(imageColor: CanvasImage | `${string}`/*.gif*/ | CustomColor): this {
+  public setBackground(imageColor: CanvasImage | `${string}.gif` | CustomColor): this {
     this.framePlacement.push({id: 1, imageColor})
     return this;
   };
@@ -42,20 +43,20 @@ export default class GifBuilder {
    * @param imageOption Source image coordinates to draw in the context of Canvas
    * @param locationOption Modify image coordinates to draw in the context of Canvas
    */
-  public setImage(image: CanvasImage | `${string}`/*.gif*/, imageOption: ImagelocationOption, locationOption?: DrawlocationOption): this {
+  public setImage(image: CanvasImage | `${string}.gif`, imageOption: ImagelocationOption, locationOption?: DrawlocationOption): this {
     this.framePlacement.push({id: 2, image, imageOption, locationOption})
     return this;
   };
 
   /**
    * Sets a predefined frame
-   * @param typeShape Frame format
+   * @param shape Frame format
    * @param coordinate Coordinate X and Y from upper left corner of the frame
    * @param size Frame size
    * @param options Frame configuration
    */
-  public setFrame<T extends FrameType, S extends Shape>(typeShape: Shape, coordinate: FrameOption<S>, options?: FrameContent<T>): this {
-    this.framePlacement.push({id: 3, typeShape, coordinate, options})
+  public setFrame<T extends FrameType, S extends Shape>(shape: S, frame: FrameOption<S>, options: FrameContent<T>): this {
+    this.framePlacement.push({id: 3, shape, frame, options})
     return this;
   };
 
@@ -78,10 +79,26 @@ export default class GifBuilder {
    * @param cloneWidth Size of the second progression bar
    * @param color Text color (a degrade can be applied with <createRadialGradient | createLinearGradient] of the Canvas module), White color is used by Default
    */
-  public setExp(option: ExpOption, progress: number, color?: CustomColor): this {
+  public setExp(option: ExpOption, progress: IntRange<0, 101>, color?: ExpColor): this {
     this.framePlacement.push({id: 5, option, progress, color})
     return this;
   };
+
+  public setFont(name: string, size?: number): this {
+
+    this.framePlacement.push({id: 6, name, size})
+    return this;
+  };
+  
+  public setLoading<D extends ShapeLoad, S extends Shape>(shape: Shape, option: LoadingOption<D, S>): this {
+    this.framePlacement.push({id: 7, shape, option})
+    return this;
+  };
+
+  public setAxis(axis: Axis): this {
+    this.framePlacement.push({id: 8, axis})
+    return this;
+  }
 
   /**
    * Return canvas Buffer
@@ -99,16 +116,17 @@ export default class GifBuilder {
 
       let imageData: Array<Image>;
 
-      if (!e.image && !e.options?.content?.imageOrText && e.imageColor) continue;
-      if (/.gif$/.test(e.image || e.options?.content?.imageOrText || e.imageColor)) {
-        imageData = await gifExtractor(e.image || e.options?.content?.imageOrText || e.imageColor)
+      // if ([0].includes(e.id)) continue
+      if (!e.image && !e.options?.content && !e.imageColor) continue;
+      if (/.gif$/.test(e.image || e.options?.content || e.imageColor)) {
+        imageData = await gifExtractor(e.image || e.options?.content || e.imageColor)
       } else if (e.imageColor && typeof e.imageColor !== "object") {
         continue;
       } else {
-        imageData = e.image || e.options?.content?.imageOrText || e.imageColor
+        imageData = e.image || e.options?.content || e.imageColor
       };
 
-      data.length < imageData.length && data.length !== 0 && typeof data == "object" && (typeof e.image?.length !== undefined || typeof e.imageColor?.length !== undefined || typeof e.options?.content?.imageOrText?.length !== 'undefined')? "" : data.length = imageData.length;
+      data.length < imageData.length && data.length !== 0 && typeof data == "object" && (typeof e.image?.length !== undefined || typeof e.imageColor?.length !== undefined || typeof e.options?.content?.length !== 'undefined')? "" : data.length = imageData.length;
 
       switch (e.id) {
         case 1: {
@@ -127,6 +145,10 @@ export default class GifBuilder {
     };
 
     const builder = new NessBuilder(this.canvas.width, this.canvas.height);
+
+    const count = this.framePlacement.filter(function(obj) {
+      return obj.id === 8;
+    }).length;
 
     for (let i = 0; i < data.length; i++) {
 
@@ -159,11 +181,11 @@ export default class GifBuilder {
           };
           case 3: {
             if (!data.setFrame[z].complete) {
-              e.options.content.imageOrText = data.setFrame[z][i];
-              builder.setFrame(e.typeShape, e.coordinate, e.size);
+              e.options.content = data.setFrame[z][i];
+              builder.setFrame(e.shape, e.frame, e.options);
             } else {
-              e.options.content.imageOrText = data.setFrame[z];
-              builder.setFrame(e.typeShape, e.coordinate, e.size);
+              e.options.content = data.setFrame[z];
+              builder.setFrame(e.shape, e.frame, e.options);
             };
             z++;
             break;
@@ -174,6 +196,19 @@ export default class GifBuilder {
           };
           case 5: {
             builder.setExp(e.option, e.progress, e.color)
+            break;
+          };
+          case 6: {
+            builder.setFont(e.name, e.size)
+            break;
+          };
+          case 7: {
+            builder.setLoading(e.shape, e.option)
+            break;
+          };
+          case 8: {
+            builder.setAxis(e.axis)
+            x++
             break;
           };
         };
